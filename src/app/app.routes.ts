023f79ea6b8d0a7f1253801b2974ff8ca8bcf277
rajeshcanaris <rajeshcanaris@gmail.com>
2024-05-07 23:23:16 +0530

    angular front end project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { LiveComponent } from './pages/live/live.component';
+import { PointTableComponent } from './pages/point-table/point-table.component';
+
+export const routes: Routes = [
+
+    {
+        path: 'home',
+        component:HomeComponent,
+        title:"Home | CrickInfomer"
+    },
+    {
+        path:"history",
+        component:HistoryComponent,
+        title:"Match History | CrickInfomer"
+
+    },
+    {
+        path:"live",
+        component:LiveComponent,
+        title:"Live Matches | CrickInfomer"
+    },
+    {
+        path:"point-table",
+        component:PointTableComponent,
+        title:"Point Table | CrickInfomer"
+    }
+];
